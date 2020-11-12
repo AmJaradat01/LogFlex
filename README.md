09ed6bddaf38eaa797eca739ae741219d45f0655
Ali Jaradat <Ali.Jaradat@mbc.net>
2020-11-12 20:24:15 +0200

    1.1.0

--- README.md
+++ README.md
@@ -2,13 +2,33 @@
 
 ## install
 ```shell
-npm i logDataToFile
+npm i logdatatofile
 ```
 
 ## Using
 ```js
-const logData = require('logDataToFile');
+const logData = require('logdatatofile');
 
-logData('output.csv', 'data1', 'data2', 'data3');
+logData('output', 'dd-mm-yyyy', '.csv', 'data1', 'data2', 'dataX');
 ```
+
+```js
+const logData = require('logdatatofile');
+let fileName = 'output';
+let fileExtension = '.csv';
+let DateTimeStampFormat = 'dd-mm-yyyy'; //['mm-dd-yyyy', 'dd-mm', 'mm-dd'] 
+
+
+logData(fileName, DateTimeStampFormat, fileExtension, 'data1', 'data2', 'dataX');
+```
+
 >At leaset you should add two data inputs
+
+```js
+logData(fileName, DateTimeStampFormat, fileExtension, dataHeader1, dataHeader2, ...dataHeaderX)
+```
+### Date TimeStamp Format Options:
+- dd-mm-yyyy
+- mm-dd-yyyy
+- dd-mm
+- mm-dd
--- index.js
+++ index.js
@@ -1,16 +1,45 @@
 const fs = require('fs');
- 
+
+let current_datetime = new Date()
+let dd_mm_yyyy = current_datetime.getDate() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getFullYear();
+let mm_dd_yyyy = (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + "-" + current_datetime.getFullYear();
+let dd_mm = current_datetime.getDate() + "-" + (current_datetime.getMonth() + 1);
+let mm_dd = (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate();
+
 /**
- * 
  * @param {String} fileName name of the file that will export {ex: result.csv || data.log etc...}
  * @param {any} dataHeader1 Mandetory data field
  * @param {any} dataHeader2 Mandetory data field
  * @param  {...any} dataHeader3 Optinal data field
- */
-module.exports = function logDataToFile(fileName, dataHeader1, dataHeader2, ...dataHeader3) {
 
-     fs.appendFile(fileName, `${dataHeader1},${dataHeader2},${dataHeader3}\n`, function (err) {
-         if (err) throw err;
-         console.log('Saved!');
-     });
+ * @param {String} fileName name of the file that will export {ex: result.csv || data.log etc...}
+ * @param {*} DateTimeStampFormat select one of these timestamps, 'dd-mm-yyyy', 'mm-dd-yyyy', 'dd-mm', 'mm-dd', or '' for no timestamp.
+ * @param {*} fileExtention put the file extenstion like '.csv', '.txt', '.log', etc...
+ * @param {any} dataHeader1 Mandatory data field
+ * @param {any} dataHeader2 Mandatory data field
+ * @param  {...any} dataHeader3 Optinal data fields
+ */
+module.exports = function logDataToFile(fileName, DateTimeStampFormat, fileExtension, dataHeader1, dataHeader2, ...dataHeaderX) {
+
+    switch (DateTimeStampFormat) {
+        case "dd-mm-yyyy":
+            DateTimeStampFormat = dd_mm_yyyy;
+            break;
+        case "mm-dd-yyyy":
+            DateTimeStampFormat = mm_dd_yyyy;
+            break;
+        case "dd-mm":
+            DateTimeStampFormat = dd_mm;
+            break;
+        case "mm-dd":
+            DateTimeStampFormat = mm_dd;
+            break;
+        default:
+            DateTimeStampFormat = '';
+            break;
+    }
+    fs.appendFile(`${fileName}${DateTimeStampFormat}${fileExtension}`, `${dataHeader1},${dataHeader2},${dataHeaderX}\n`, function (err) {
+        if (err) throw err;
+        //console.log('Saved!');
+    });
 }
